519b2b7c2498055db41f4c3f92af28d5937b2940
Santiago Marin Cardona <marincardona.santiago@hotmail.com>
2025-10-01 10:16:06 -0500

    intento1

--- homework/queries.py
+++ homework/queries.py
@@ -3,6 +3,110 @@
 # pylint: disable=broad-exception-raised
 # pylint: disable=import-error
 
+from homework.mapreduce import mapreduce  # type: ignore
+import shutil
+import os
+#
+# Columns:
+# total_bill, tip, sex, smoker, day, time, size
+#
+
+#
+# QUERY 1:
+# SELECT *, tip/total_bill as tip_rate
+# FROM tips;
+#
+
+def mapper_query_1(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append(
+                (index, row.strip() + ",tip_rate")
+            )
+        else:
+            row_values = row.strip().split(",")
+            total_bill = float(row_values[0])
+            tip = float(row_values[1])
+            tip_rate = tip / total_bill
+            result.append((index, row.strip() + "," + str(tip_rate)))
+    return result
+
+
+def reducer_query_1(sequence):
+    """Reducer"""
+    return sequence
+
+
+#
+# SELECT *
+# FROM tips
+# WHERE time = 'Dinner';
+#
+def mapper_query_2(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append((index, row.strip()))
+        else:
+            row_values = row.strip().split(",")
+            if row_values[5] == "Dinner":
+                result.append((index, row.strip()))
+    return result
+
+
+def reducer_query_2(sequence):
+    """Reducer"""
+    return sequence
+
+#
+# SELECT *
+# FROM tips
+# WHERE time = 'Dinner' AND tip > 5.00;
+#
+def mapper_query_3(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append((index, row.strip()))
+        else:
+            row_values = row.strip().split(",")
+            if row_values[5] == "Dinner" and float(row_values[1]) > 5.00:
+                result.append((index, row.strip()))
+    return result
+
+
+def reducer_query_3(sequence):
+    """Reducer"""
+    return sequence
+
+
+
+#
+# SELECT *
+# FROM tips
+# WHERE size >= 5 OR total_bill > 45;
+#
+def mapper_query_4(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append((index, row.strip()))
+        else:
+            row_values = row.strip().split(",")
+            if int(row_values[6]) >= 5 or float(row_values[0]) > 45:
+                result.append((index, row.strip()))
+    return result
+
+
+def reducer_query_4(sequence):
+    """Reducer"""
+    return sequence
+
 
 #
 # ORQUESTADOR:
@@ -10,7 +114,51 @@
 def run():
     """Orquestador"""
 
+    if os.path.exists("files/query_1"):
+            shutil.rmtree("files/query_1")
+
+    mapreduce(
+        input_folder="files/input/", 
+        output_folder="files/query_1", 
+        mapper_fn=mapper_query_1, 
+        reducer_fn=reducer_query_1,
+    )
+
+
+    if os.path.exists("files/query_2"):
+        shutil.rmtree("files/query_2")
+
+    mapreduce(
+        input_folder="files/input/", 
+        output_folder="files/query_2", 
+        mapper_fn=mapper_query_2, 
+        reducer_fn=reducer_query_2,
+    )    
+
+    if os.path.exists("files/query_3"):
+        shutil.rmtree("files/query_3")
+
+    mapreduce(
+        input_folder="files/input/", 
+        output_folder="files/query_3", 
+        mapper_fn=mapper_query_3, 
+        reducer_fn=reducer_query_3,
+    )    
+
+
+    if os.path.exists("files/query_4"):
+        shutil.rmtree("files/query_4")
+
+    mapreduce(
+        input_folder="files/input/", 
+        output_folder="files/query_4", 
+        mapper_fn=mapper_query_4, 
+        reducer_fn=reducer_query_4,
+    )    
+
 
 if __name__ == "__main__":
 
     run()
+
+    
